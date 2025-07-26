4ea989802cb2beac14c523a59a562f226a58be33
Megha Singh <meghasingh2k24@gmail.com>
2025-07-26 23:05:09 +0530

    cookies

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ const storeRouter = require("./routes/storeRouter")
 const hostRouter = require("./routes/hostRouter")
 const rootDir = require("./utils/pathUtil");
 const errorsController = require("./controllers/errors");
-const {mongoConnect} = require('./utils/databaseUtil');
-
+const mongoose = require('mongoose');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -25,8 +24,18 @@ app.use(express.static(path.join(rootDir, 'public')))
 app.use(errorsController.pageNotFound);
 
 const PORT = 3000;
-mongoConnect(() => {
+// mongoConnect(() => {
+//   app.listen(PORT, () => {
+//     console.log(`Server running on address http://localhost:${PORT}`);
+//   });
+// })
+const DB_PATH="mongodb+srv://user:root@airbnb.ysia5nq.mongodb.net/airbnb?retryWrites=true&w=majority&appName=airbnb"
+mongoose.connect(DB_PATH).then(() => {
+  console.log("Connected to MongoDB");
   app.listen(PORT, () => {
     console.log(`Server running on address http://localhost:${PORT}`);
   });
-})
+
+}).catch((err) => {
+  console.error("Failed to connect to MongoDB", err);
+});
